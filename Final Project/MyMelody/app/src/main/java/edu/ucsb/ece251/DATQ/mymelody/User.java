dde309b34f0ccbac253504cf08a1ec2a0fd28d03
Dhruv Aggarwal <dna@ucsb.edu>
2023-11-28 21:25:24 -0800

    added new files for user/styling

---
+++ Final Project/MyMelody/app/src/main/java/edu/ucsb/ece251/DATQ/mymelody/User.java
@@ -0,0 +1,69 @@
+package edu.ucsb.ece251.DATQ.mymelody;
+
+import android.text.Html;
+
+public class User {
+    private String id;
+    private String username;
+    private String email;
+    private String profileLink;
+    private String pfpLink;
+
+    public User() {
+        this.id = "";
+        this.username= "";
+        this.email= "";
+        this.profileLink= "";
+        this.pfpLink= "";
+    }
+    // Constructor
+    public User(String id, String username, String email, String profileLink, String pfpLink) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.profileLink = profileLink;
+        this.pfpLink = pfpLink;
+    }
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getProfileLink() {
+        return profileLink;
+    }
+    public String getPFPLink() {
+        return pfpLink;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setProfileLink(String profileLink) {
+        this.profileLink = profileLink;
+    }
+    public void setPFPLink(String pfpLink) {
+        this.pfpLink = pfpLink;
+    }
+    // Optional: Override toString() for easy printing of User object details
+    @Override
+    public String toString() {
+        return "Username: " + username + "\n" +
+                "id: " + id + "\n" +
+                "Email: " + email + "\n" +
+                Html.fromHtml("<a href=" + profileLink + "> Profile </a>")
+                ;
+    }
+}
